503c4d0fc50579875c914cd5d7e929f651609cb6
Max Tuni <maxilandia@gmail.com>
2014-05-14 17:38:41 +0200

    sleep with timeout

--- src/main/java/es/predictia/util/Timeouts.java
+++ src/main/java/es/predictia/util/Timeouts.java
@@ -7,8 +7,45 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Timeouts {
 
+	public interface AwakeningCondition{
+		public boolean wakeUp() throws Exception;
+	}
+	
+	/** Sleeps checking for an {@link AwakeningCondition}, with a maximum sleeping time (timeOut)
+	 * @param sleepInterval time to sleep (in millis) untill checking for {@link AwakeningCondition#wakeUp()}
+	 * @param condition
+	 * @param timeOut
+	 * @param timeOutUnit
+	 * @throws InterruptedException
+	 * @throws ExecutionException
+	 */
+	public static void sleepWithTimeOut(final long sleepInterval, final AwakeningCondition condition, long timeOut, TimeUnit timeOutUnit) throws InterruptedException, ExecutionException{
+		Exception e = Timeouts.callWithTimeOut(new Callable<Exception>() {
+			@Override
+			public Exception call() throws Exception {
+				try{
+					do{
+						Thread.sleep(sleepInterval);
+					}while(!condition.wakeUp());
+				}catch(Exception e){
+					LOGGER.warn("Exception while waiting for process: " + e.getMessage());
+					return e;
+				}
+				return null;
+			}
+		}, timeOut, timeOutUnit);
+		if(e instanceof InterruptedException){
+			throw (InterruptedException) e;
+		}else if(e instanceof Exception){
+			throw new ExecutionException(e);
+		}
+	}
+	
 	/**
 	 * @param task
 	 * @param timeout
@@ -48,5 +85,6 @@ public class Timeouts {
 			executorService.shutdownNow();
 		}
 	}
+	private static final Logger LOGGER = LoggerFactory.getLogger(Timeouts.class);
 	
 }
